a786486425fed93d3c8bb3e8e6c6c51b8955c7ba
bielgirao <bielgirao@hotmail.com>
2025-02-12 22:38:37 -0300

    feat: add app routes initial structure

---
+++ routes/user.routes.js
@@ -0,0 +1,11 @@
+import {Router} from 'express';
+
+const userRouter = Router();
+
+userRouter.get('/', (req, res) => res.send({ title: 'GET All Users' }));
+userRouter.get('/:id', (req, res) => res.send({title: 'GET User details'}));
+userRouter.post('/', (req, res) => res.send({title: 'CREATE User'}));
+userRouter.put('/:id', (req, res) => res.send({title: 'UPDATE User'}));
+userRouter.delete('/:id', (req, res) => res.send({title: 'DELETE User'}));
+
+export default userRouter;
